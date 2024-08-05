fd09f034df14942edd3bbed812829aa39b8d47d3
dnaorem <dnaorem@XTV-IT-DNAOREM>
2024-08-05 22:19:12 +0530

    Prop file changed

--- ConnectKafka/src/main/java/com/kafka/connect/GitPushExample.java
+++ ConnectKafka/src/main/java/com/kafka/connect/GitPushExample.java
@@ -2,6 +2,14 @@ package com.kafka.connect;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.Status;
@@ -48,6 +56,9 @@ public class GitPushExample {
                     .call();
 
             System.out.println("Changes pushed to remote repository successfully!");
+            
+            createPullRequest(token);
+            System.out.println("Sent Pull Request to reviwer!");
 
         } catch (IOException e) {
             logger.error("IOException occurred: ", e);
@@ -55,4 +66,48 @@ public class GitPushExample {
             logger.error("GitAPIException occurred: ", e);
         }
     }
+    
+    public void createPullRequest(String token) {
+    	HttpURLConnection connection = null;
+    	try {
+    		URL obj = new URI("https://api.github.com/repos/dnaorem/GitHubActionsDemo/pulls").toURL();
+    		connection = (HttpURLConnection)obj.openConnection();
+    		
+    		connection.setRequestMethod("POST");
+            connection.setDoOutput(true);
+
+            connection.setRequestProperty("Authorization", "Bearer " + token);
+            connection.setRequestProperty("Accept", "application/vnd.github+json");
+            connection.setRequestProperty("X-GitHub-Api-Version", "2022-11-28");
+    		
+            try (DataOutputStream os = new DataOutputStream(connection.getOutputStream())) {
+                os.writeBytes("{\"title\":\"New Topic Added by Developer\",\"body\":\"Please pull these new changes in!\",\"head\":\"dnaorem:Test\",\"base\":\"main\"}");
+                os.flush();
+            }
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                StringBuilder response = new StringBuilder();
+
+                try (
+                    BufferedReader reader = new BufferedReader( new InputStreamReader( connection.getInputStream()))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line); 
+                    }
+                }
+                System.out.println("Response: " + response.toString());
+            }
+            else {
+                System.out.println("Error: HTTP Response code - " + responseCode);
+            }
+    	} catch (Exception e) {
+    		
+    	} finally {
+    		if (connection != null) {
+    			connection.disconnect();
+    		}
+    	}
+    }
+    
 }
